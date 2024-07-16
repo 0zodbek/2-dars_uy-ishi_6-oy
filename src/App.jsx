7b5c9a1a4f40061d3c8d1687a0cb9a66285d8e83
0zodbek <ozpdbekov@gmail.com>
2024-07-17 04:02:14 +0500

    k

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Card from './assets/components/Card'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    
+      <>
+      <Card />
+        <h1>hello world</h1>
+      </>
+    
+  )
+}
+
+export default App
