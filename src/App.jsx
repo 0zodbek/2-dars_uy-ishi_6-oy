26a9e6789272fcb35a68d25fb1d1cea72837528d
0zodbek <ozpdbekov@gmail.com>
2024-07-17 11:08:20 +0500

    gitpush

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import react from 'react'
 import './App.css'
 import Card from './assets/components/Card'
 import data from './assets/data.json'
 function App() {
   // const [count, setCount] = useState(0)
-// console.log(data);
+console.log(data);
   return (
-          <div>
+          <div className='wrapper'>
         {
-          data.length && data.map((el,index) => {
-           return <Card Key = {index} data = {el}></Card>
+        data.length && data.map((el,index) => {
+          return <Card key = {index} data = {el}></Card>
+          console.log(el);
           })
         }
       </div>
--- src/assets/components/Card/index.jsx
+++ src/assets/components/Card/index.jsx
@@ -1,25 +1,18 @@
 import React from 'react'
-
+import './index.css'
 function Card(props) {
-    const { data } = props;
-
+        const {data} = props;
+        console.log(data.firstname);
     return (
         <div className="card">
-
-            <img src="https://picsum.photos/200?random=1" alt="bu yerda rasm" />
-
+            <img src={data.image} alt="bu yerda rasm" />
             <div className="part_info">
-
-                <h2>{data.name}</h2>
-
-                <div className="phone_email_wrapper"><h3>{data.phone},{data.email}  email:Therese.Boehm3@gmail.com</h3></div>
-
-                <h3 className="address">{data.address}</h3>
-
-                <h3 className="name">{data.cars}</h3>
-
+                <h2>{data.firstName}  {data.lastName}</h2>
+                <h2>{data.name}</h2><br />
+                <div className="phone_email_wrapper"><h3>{data.phone} <br /> {data.email}</h3></div><br />
+                {/* <h3 className="address">{data.address}</h3> */}
+                <h3 className="name">{data.cars[0]}  {data.cars[1]}  {data.cars[2]}</h3><br />
             </div>
-
         </div>
     )
 }
